7760d5349570b9cf12fd212a7a6c215222e8bc90
johncllstokes <johnclstokes@hotmail.com>
2023-12-01 21:31:23 +0000

    initial project setup

---
+++ day_1/target/debug/deps/day_1_project-051d1a23363a4e6b.d
@@ -0,0 +1,5 @@
+C:\Users\johnc\advent_of_code_2023\day_1\target\debug\deps\libday_1_project-051d1a23363a4e6b.rmeta: src\main.rs
+
+C:\Users\johnc\advent_of_code_2023\day_1\target\debug\deps\day_1_project-051d1a23363a4e6b.d: src\main.rs
+
+src\main.rs:
---
+++ day_1/target/debug/deps/day_1_project-c2efca6396950ac9.d
@@ -0,0 +1,5 @@
+C:\Users\johnc\advent_of_code_2023\day_1\target\debug\deps\libday_1_project-c2efca6396950ac9.rmeta: src\main.rs
+
+C:\Users\johnc\advent_of_code_2023\day_1\target\debug\deps\day_1_project-c2efca6396950ac9.d: src\main.rs
+
+src\main.rs:
