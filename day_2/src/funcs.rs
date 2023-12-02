4767510f6482117d6979891030a472fadb65957c
johncllstokes <johnclstokes@hotmail.com>
2023-12-02 14:38:19 +0000

    further itterating towards the solution

--- day_2/src/funcs.rs
+++ day_2/src/funcs.rs
@@ -2,8 +2,8 @@ use regex::Regex;
 
 /// return a boolean if the game represents a possible occurance of cubes
 /// else, return false
-fn is_valid(input_string: &str) -> bool {
-    let re = Regex::new(r"XXX").unwrap();
+fn is_valid(input_string: &str, substring: &str) -> bool {
+    let re = Regex::new(&format!(r#"(?i){}\s*(\d+)"#, substring)).unwrap();
     let cube_count: Vec<&str> = re.find_iter(input_string).map(|m| m.as_str()).collect();
     // if any is > x return false, else return true
     return false
@@ -22,6 +22,11 @@ fn bool_to_game_numb(input_string: &str, validity: bool) -> i32 {
 
 /// convert game string to valid int output
 pub fn game_count(input_string: &str) -> i32 {
-    let valid_game_check = is_valid(input_string);
-    return bool_to_game_numb(input_string, valid_game_check)
+    let valid_blue = is_valid(input_string, "blue");
+    let valid_green = is_valid(input_string, "green");
+    let valid_red = is_valid(input_string, "red");
+
+    let validity = valid_blue && valid_green && valid_red;
+
+    return bool_to_game_numb(input_string, validity)
 }
